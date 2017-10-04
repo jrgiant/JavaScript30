d467fde37bc5865f11c623b9f3f6093a23725392
jrgiant <jrgiant_git@outlook.com>
2017-10-03 20:02:31 -0400

    Completion of Module 19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -5,6 +5,86 @@ const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
 function getVideo() {
-  navigator.mediaDevices.getUserMedia({video:true, audio:false});
-  
+  navigator.mediaDevices.getUserMedia({video:true, audio:false})
+  .then(localMediaStream => {
+    console.log(localMediaStream);
+    video.src = window.URL.createObjectURL(localMediaStream);
+    video.play();
+  }).catch((err) => {console.error(`OH Noooooo!!!!!!!!!!!!`, err)});
 }
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+   return setInterval(function () {
+    ctx.drawImage(video,0,0,width,height);
+    let pixels = ctx.getImageData(0,0,width,height);
+    //red effect
+    //pixels = redEffect(pixels);
+    //rgbSplit
+    // pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.1;
+    pixels = greenScreen(pixels);
+    ctx.putImageData(pixels,0,0);
+  }, 16);
+}
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'Smile');
+  link.techContent = 'download';
+  link.innerHTML = `<img src="${data}" alt="Your Pic" />`;
+  strip.insertBefore(link, strip.firstChild);
+
+}
+getVideo();
+function redEffect(pixels) {
+  for (var i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i + 0 ]+= 300;//r
+    pixels.data[i + 1 ]+=50//g
+    pixels.data[i + 2 ]-=20//b
+
+  }
+  return  pixels;
+}
+function rgbSplit(pixels) {
+  for (var i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i - 150 ] =pixels.data[i + 0 ]
+    pixels.data[i + 500 ]=pixels.data[i + 1 ]
+    pixels.data[i - 550 ]= pixels.data[i + 2 ]
+
+  }
+  return  pixels;
+}
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
+}
+video.addEventListener('canplay', paintToCanvas);
